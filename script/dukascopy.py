8c022c3d9179676b349fb7731fb4bed263b95349
dot dash <xxyxzxxyzyxxxzxyzxyx@gmail.com>
2021-02-21 22:00:34 +0900

    rm class, make defs

--- script/dukascopy.py
+++ script/dukascopy.py
@@ -7,43 +7,50 @@ from io import BytesIO, DEFAULT_BUFFER_SIZE
 from lzma import LZMADecompressor, LZMAError, FORMAT_AUTO
 
 
-class Dukascopy(object):
-    def __init__(self, pair, year, month, day):
-        self.pair = pair
-        self.year = year
-        self.month = month
-        self.day = day
-        self.urls = [
-            f'https://www.dukascopy.com/datafeed'
-            f'/{pair}/{year}/{month:02d}/{day:02d}/{hour:02d}'
-            f'h_ticks.bi5'
-            for hour in range(0, 24)
-        ]
+def get_requests(pair, year, month, day, filename):
+    urls = [
+        f'https://www.dukascopy.com/datafeed'
+        f'/{pair}/{year}/{month:02d}/{day:02d}/{hour:02d}'
+        f'h_ticks.bi5'
+        for hour in range(0, 24)
+    ]
 
+    request_day = BytesIO()
 
-    def get_requests(self):
-        request_day = BytesIO()
-        check = []
+    for url in urls:
+        request_hour = BytesIO()
+        request = requests.get(url)
+        check_request = request.status_code
+        check_buffer = 0
 
-        for url in self.urls:
-            request_hour = BytesIO()
-            request = requests.get(url)
-            check_request = request.status_code
-            check_buffer = 0
+        if check_request == 200:
+            for request_buffer in request.iter_content(DEFAULT_BUFFER_SIZE):
+                request_hour.write(request_buffer)
 
-            if check_request == 200:
-                for request_buffer in request.iter_content(DEFAULT_BUFFER_SIZE):
-                    request_hour.write(request_buffer)
+            buffer_hour = request_buffer.getbuffer()
+            check_buffer = len(buffer_hour)
 
-                buffer_hour = request_buffer.getbuffer()
-                check_buffer = len(buffer_hour)
-
-                if 0 < check_buffer:
-                    request_day.write(buffer_hour)
-                else:
-                    pass
-
+            if 0 < check_buffer:
+                request_day.write(buffer_hour)
             else:
                 pass
 
-            check.append((url, check_request, check_buffer))
+        else:
+            pass
+    
+    with open(f'{filename}', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow((url, check_request, check_buffer))
+
+    return request_day.getbuffer() 
+
+
+def decomp_buffer(buffer):
+
+
+
+
+def write_ticks(stream):
+
+
+
