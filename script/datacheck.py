145e969d08b30d1a4dcc15f88ac2d9e19f1818b4
dot dash <xxyxzxxyzyxxxzxyzxyx@gmail.com>
2021-02-26 01:26:51 +0900

    temp commit i am sleepy

--- script/datacheck.py
+++ script/datacheck.py
@@ -9,13 +9,41 @@ class Datacheck(object):
         self.latest = f'{logdir}/latest'
         self.logs = glob.glob(f'{logdir}/*.log')
 
+
     def logsummary(self):
         try:
             latest = pd.read_csv(self.latest)
         except FileNotFoundError:
-            latest = pd.DataFrame(columns=['request_url', 'year', 'month', 'day', 'hour', 'status_code', 'buffer_size'])
+            latest = pd.DataFrame(
+                columns=[
+                    'request_url',
+                    'year',
+                    'month',
+                    'day',
+                    'hour',
+                    'status_code',
+                    'buffer_size',
+                    'execute_time'
+                ]
+            )
 
         for logpath in logs:
             log = pd.read_csv(logpath)
             os.renames(f'{i}', f'{self.logdir}/old/{logpath.split("/").pop()}')
             latest = pd.concat([latest, log])
+
+        latestlog = latest.sort_values(
+            by=['year', 'month', 'day', 'hour', 'execute_time'],
+            ascending=True
+        ).drop_duplicates(
+            subset=['request_url'],
+            keep='last'
+        ).reset_index(
+            drop=True
+        )
+
+        latestlog.to_csv(f'{self.logdir}/latest', index=False)
+
+        return latestlog
+
+
